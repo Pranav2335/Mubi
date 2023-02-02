563c6ccec197d8ef9296fbfe38b305879fbf1676
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2023-02-02 14:12:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  gh repo clone matrixbotz0/PETER-PARKERBOT
+  gh repo clone https://github.com/Pranav2335/IMDB-prvbot.git /IMDB-prvbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PETER-PARKERBOT
+  git clone $UPSTREAM_REPO /IMDB-prvbot
 fi
-cd /PETER-PARKERBOT
+cd /IMDB-prvbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
 python3 bot.py
