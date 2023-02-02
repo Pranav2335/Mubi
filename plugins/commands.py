f2903779dabeeff5bfd1682858e8158b00c9c453
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2023-02-02 15:11:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -88,13 +88,13 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("➕️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="https://t.me/kmtz_channel_v3")
+            InlineKeyboardButton("🔍 ꜱᴇᴀʀᴄʜ 🔍", switch_inline_query_current_chat=''), 
+            InlineKeyboardButton("📢 ᴜᴩᴅᴀᴛᴇꜱ 📢", url="https://t.me/kmtz_channel_v3")
             ],[      
-            InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿 ℹ️", callback_data="help"),
-            InlineKeyboardButton("💫 𝙰𝙱𝙾𝚄𝚃 💫", callback_data="about")
+            InlineKeyboardButton("ℹ️ ʜᴇʟᴘ ℹ️", callback_data="help"),
+            InlineKeyboardButton("💫 ᴀʙᴏᴜᴛ 💫", callback_data="about")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
